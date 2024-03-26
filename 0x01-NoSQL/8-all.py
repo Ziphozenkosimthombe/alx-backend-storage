001c4029b19883286eb1f018243c7320b774ae4b
Ziphozenkosimthombe <ziphomthombe478@gmaile.com>
2024-03-26 03:54:04 -0700

    8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
-
-""" list all docs"""
+"""
+    8-all.py
+"""
 
 
 def list_all(mongo_collection) -> list:
+    """list all documents in a collection"""
     return mongo_collection.find()
